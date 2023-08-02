40c518ce5513283a606722736422ddff7499a09e
彭克锐 <pengkerui@wshifu.com>
2023-08-02 17:27:25 +0800

    显示到MacOS状态栏；dock栏隐藏icon

---
+++ macos/Runner/StatusBarController.swift
@@ -0,0 +1,60 @@
+//
+//  StatusBarController.swift
+//  Runner
+//
+//  Created by 彭克锐 on 2023/8/2.
+//
+
+import AppKit
+
+class StatusBarController {
+    // Instance of the status bar
+    private var appStatusBar: NSStatusBar
+
+    // Instance of the status bar item
+    private var statusBarMenuItem: NSStatusItem
+
+    // Instance of the popover that will display the Flutter UI
+    private var flutterUIPopover: NSPopover
+
+    // Initializer for the StatusBarController class
+    init(_ popover: NSPopover) {
+        self.flutterUIPopover = popover
+        appStatusBar = NSStatusBar.init()
+        statusBarMenuItem = appStatusBar.statusItem(withLength: 28.0)
+
+        // Configure the status bar item's button
+        if let statusBarMenuButton = statusBarMenuItem.button {
+            // Set the button's image
+            statusBarMenuButton.image = NSImage(named: "flutter") // Change this to your desired image
+            statusBarMenuButton.image?.size = NSSize(width: 18.0, height: 18.0)
+            statusBarMenuButton.image?.isTemplate = true
+
+            // Set the button's action to toggle the popover when clicked
+            statusBarMenuButton.action = #selector(togglePopover(sender:))
+            statusBarMenuButton.target = self
+        }
+    }
+
+    // Function to toggle the popover when the status bar item's button is clicked
+    @objc func togglePopover(sender: AnyObject) {
+        if(flutterUIPopover.isShown) {
+            hidePopover(sender)
+        }
+        else {
+            showPopover(sender)
+        }
+    }
+
+    // Function to show the popover
+    func showPopover(_ sender: AnyObject) {
+        if let statusBarMenuButton = statusBarMenuItem.button {
+            flutterUIPopover.show(relativeTo: statusBarMenuButton.bounds, of: statusBarMenuButton, preferredEdge: NSRectEdge.maxY)
+        }
+    }
+
+    // Function to hide the popover
+    func hidePopover(_ sender: AnyObject) {
+        flutterUIPopover.performClose(sender)
+    }
+}
